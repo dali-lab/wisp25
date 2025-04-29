87cebeb6f55d57b7cd331eba69a591bf77c98ac8
fruityysocks <prishita.t.dharampal.28@dartmouth.edu>
2025-04-28 20:55:10 -0400

    created a placeholder form to add posting functionality to app

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import LandingPage from '../screens/LandingPage/LandingPage';
-import CourseSearch from '../screens/CourseSearch';
-import SavedCourses from '../screens/SavedCourses';
-import CourseLoadTester from '../screens/CourseLoadTester';
-import CoffeeChat from '../screens/CoffeeChat/CoffeeChat';
-import ExploreCoffeeChat from '../screens/CoffeeChat/ExploreCoffeeChat';
-import RegisterCoffeeChat from '../screens/CoffeeChat/RegisterCoffeeChat';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LandingPage from "../screens/LandingPage/LandingPage";
+import CourseSearch from "../screens/CourseSearch";
+import SavedCourses from "../screens/SavedCourses";
+import CourseLoadTester from "../screens/CourseLoadTester";
+import CoffeeChat from "../screens/CoffeeChat/CoffeeChat";
+import ExploreCoffeeChat from "../screens/CoffeeChat/ExploreCoffeeChat";
+import RegisterCoffeeChat from "../screens/CoffeeChat/RegisterCoffeeChat";
+import RegisterPlaceholder from "../screens/CoffeeChat/RegisterPlaceholder";
 
 const App: React.FC = () => {
   return (
@@ -37,8 +38,9 @@ const App: React.FC = () => {
           <Route path="/savedCourses" element={<SavedCourses />} />
           <Route path="/courseloadtester" element={<CourseLoadTester />} />
           <Route path="coffeechat" element={<CoffeeChat />}>
-            <Route path="registercoffeechat" element={<RegisterCoffeeChat />} />
-            <Route path="explorecoffeechat" element={<ExploreCoffeeChat />} />
+          <Route path="registercoffeechat" element={<RegisterCoffeeChat />} />
+          <Route path="explorecoffeechat" element={<ExploreCoffeeChat />} />
+          <Route path="registercoffeechattest" element={< RegisterPlaceholder/>} />
           </Route>
         </Routes>
       </div>
